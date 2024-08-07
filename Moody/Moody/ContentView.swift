3e83700046d51b4da7a12cf07ae8c7c8204a973e
BenjaminSyntax <155439470+BenjaminSyntax@users.noreply.github.com>
2024-08-07 11:00:03 +0200

    Initial commit

---
+++ Moody/Moody/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moody
+//
+//  Created by Benjamin Moritz on 07.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello Du, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moody/Moody/MoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyApp.swift
+//  Moody
+//
+//  Created by Benjamin Moritz on 07.08.24.
+//
+
+import SwiftUI
+
+@main
+struct MoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
